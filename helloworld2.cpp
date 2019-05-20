027d36fbe07853527ee3ba84c1dca4dcd29b6567
Gary Segal <segal@synopsys.com>
2019-05-20 14:40:29 -0400

    commit

--- helloworld2.cpp
+++ helloworld2.cpp
@@ -8,6 +8,6 @@ int print_something ()
   cout << "Printing something else changed locally.\n";
   cout << "Here's another local change.\n";
   cout << "A local change for a git push.\n";
-  cout << "A local change git pull request.\n"
+  cout << "A local change git pull request.\n";
   return 0;
 }
