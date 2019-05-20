0214f52e5c1f04d2c8d978bf1720a9d55bdcd050
Gary Segal <segal@synopsys.com>
2019-05-20 14:35:12 -0400

    commit for a git pull request

--- helloworld2.cpp
+++ helloworld2.cpp
@@ -7,6 +7,7 @@ int print_something ()
 {
   cout << "Printing something else changed locally.\n";
   cout << "Here's another local change.\n";
-  cout << "A local change for a git push.";
+  cout << "A local change for a git push.\n";
+  cout << "A local change git pull request.\n"
   return 0;
 }
